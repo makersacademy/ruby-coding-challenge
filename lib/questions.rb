6fe158d9f932ff2f308ddd8246fd0ae539938c12
Nikesh Ashar <nikeshashar@gmail.com>
2017-09-21 12:07:10 +0100

    First round of splitting up problems

---
+++ lib/questions.rb
@@ -0,0 +1,14 @@
+# turn a positive integer into a negative integer. A negative integer
+# stays negative
+def make_numbers_negative(number)
+end
+
+# round up a float up and convert it to an Integer,
+# so 3.214 becomes 4
+def round_up_number(float)
+end
+
+# round down a float up and convert it to an Integer,
+# so 9.52 becomes 9
+def round_down_number(float)
+end
