1f14a1e915ebf261ebf87ae3038d733a64f0caea
Nikesh Ashar <nikeshashar@gmail.com>
2017-09-21 17:51:24 +0100

    Ordering in more questions after passing tests

--- lib/questions.rb
+++ lib/questions.rb
@@ -12,3 +12,38 @@ end
 # so 9.52 becomes 9
 def round_down_number(float)
 end
+
+# swap the keys and values in a hash. e.g.
+# {'a' => 'b', 'c' => 'd'} becomes
+# {'b' => 'a', 'd' => 'c'}
+def swap_keys_and_values_in_a_hash(hash)
+end
+
+# in a hash where the keys and values are all numbers
+# add all the keys and all the values together, e.g.
+# {1 => 1, 2 => 2} becomes 6
+def add_together_keys_and_values(hash)
+end
+
+# turn an array (with an even number of elements) into a hash, by
+# pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
+# {'a' => 'b', 'c' => 'd'}
+def convert_array_to_a_hash(array)
+end
+
+
+# take out all the capital letters from a string
+# so 'Hello JohnDoe' becomes 'ello ohnoe'
+def remove_capital_letters_from_string(string)
+end
+
+
+# cut strings in half, and return the first half, e.g.
+# 'banana' becomes 'ban'. If the string is an odd number of letters
+# round up - so 'apple' becomes 'app'
+def get_first_half_of_string(string)
+end
+
+# convert a symbol into a string
+def turn_symbol_into_string(symbol)
+end
