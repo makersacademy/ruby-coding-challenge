f8a41b67838e5612c815577769de1346a63089a6
Anthony Crisp <anthony_crisp@hotmail.com>
2017-12-20 12:04:22 +0000

    Challenge 8

--- lib/questions.rb
+++ lib/questions.rb
@@ -50,11 +50,12 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-  
+  string[0, (string.length + 1)/2]
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  
 end
 
 
