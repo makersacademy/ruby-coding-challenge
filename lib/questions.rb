e9f960e3ebc8f96cc421b3fe79106b920d517758
Anthony Crisp <anthony_crisp@hotmail.com>
2017-12-20 13:30:19 +0000

    Challenge 27

--- lib/questions.rb
+++ lib/questions.rb
@@ -175,6 +175,7 @@ end
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+  range.exclude_end?
 end
 
 # get the square root of a number
