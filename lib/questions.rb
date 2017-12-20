382062769d106e18e9e64984f0cea0102ed55766
Anthony Crisp <anthony_crisp@hotmail.com>
2017-12-20 12:18:36 +0000

    Challenge 11

--- lib/questions.rb
+++ lib/questions.rb
@@ -79,6 +79,7 @@ end
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
+  !!string.match(/[^a-zA-Z0-9]/)
 end
 
 # keep only the elements that start with an a
