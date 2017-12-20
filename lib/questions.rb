8705dbcf1088a2bf09109173c253af1a3b278783
Anthony Crisp <anthony_crisp@hotmail.com>
2017-12-20 12:41:19 +0000

    Challenge 15

--- lib/questions.rb
+++ lib/questions.rb
@@ -100,6 +100,7 @@ end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.compact.keep_if{ |x| x }
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
