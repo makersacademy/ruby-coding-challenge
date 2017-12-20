1a212407bf4742a9ab1a8065ab228887ce95bd6c
Anthony Crisp <anthony_crisp@hotmail.com>
2017-12-20 11:59:56 +0000

    Challenge 4

--- lib/questions.rb
+++ lib/questions.rb
@@ -21,13 +21,14 @@ end
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
-  
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  
 end
 
 # turn an array (with an even number of elements) into a hash, by
