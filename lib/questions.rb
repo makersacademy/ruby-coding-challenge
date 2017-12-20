8172239de36a334dab3540d4441f31658c074a47
Anthony Crisp <anthony_crisp@hotmail.com>
2017-12-20 12:50:04 +0000

    Challenge 18

--- lib/questions.rb
+++ lib/questions.rb
@@ -117,6 +117,7 @@ end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  [element, array].flatten
 end
 
 # return the shortest word in an array
