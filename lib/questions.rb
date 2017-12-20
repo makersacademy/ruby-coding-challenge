5dc949d444388780584b251f2da6923db7a2cfe9
Anthony Crisp <anthony_crisp@hotmail.com>
2017-12-20 15:03:55 +0000

    Challenge 33

--- lib/questions.rb
+++ lib/questions.rb
@@ -224,4 +224,5 @@ end
 # and 1 that is 4 letters long. Return it as a hash in the format
 # word_length => count, e.g. {2 => 1, 3 => 5, 4 => 1}
 def count_words_of_each_length_in_a_file(file_path)
+  IO.read(file_path).gsub(/[^a-zA-Z ]/, "").split.map{ |x| x.size }.each_with_object(Hash.new(0)) { |letter, hash| hash[letter] += 1}
 end
