9403a3283bbdeb1c9089044f493d9d9d6bfabbc9
Sunali <sunaliseth01@gmail.com>
2017-12-22 22:57:45 +0000

    Test passes 'cut strings in half, and return the first half'

--- lib/questions.rb
+++ lib/questions.rb
@@ -50,6 +50,7 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+    string.chars.each_slice(3).map(&:join).first
 end
 
 # convert a symbol into a string
