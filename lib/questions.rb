728d354e9d64579fd97004ee242f1892739981ea
David Divilly <d.divilly@gmail.com>
2017-12-24 00:50:38 +0000

    11) titleize_a_string

--- lib/questions.rb
+++ lib/questions.rb
@@ -73,6 +73,12 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+  exceptions = ['a', 'and', 'the']
+  string.capitalize!
+  array = string.split(' ')
+  array.map do | str |
+    exceptions.include?(str) ? str : str.capitalize
+  end.join(' ')
 end
 
 # return true if a string contains any special characters
