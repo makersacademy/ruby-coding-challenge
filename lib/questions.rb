218e063d4777a2bad2143f119a17e251333947b7
David Divilly <d.divilly@gmail.com>
2017-12-27 18:41:01 +0000

    19) add_element_to_beginning_of_array

--- lib/questions.rb
+++ lib/questions.rb
@@ -128,6 +128,7 @@ end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # return the shortest word in an array
