17bc55ebceaa9ce171b6fc4e45c4f059b3fd9abf
David Divilly <d.divilly@gmail.com>
2017-12-27 18:50:45 +0000

    21) longest_word_in_array

--- lib/questions.rb
+++ lib/questions.rb
@@ -138,6 +138,7 @@ end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+  array.sort_by { | word | word.size }[-1]
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
