dc7300bd60fc602c0e1694edef7d703e0ac8ecad
David Divilly <d.divilly@gmail.com>
2017-12-27 17:47:17 +0000

    18) all_elements_except_first_3

--- lib/questions.rb
+++ lib/questions.rb
@@ -123,6 +123,7 @@ end
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+  array[3..-1]
 end
 
 # add an element to the beginning of an array
