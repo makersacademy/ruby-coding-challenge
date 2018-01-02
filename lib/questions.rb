8afc908500e7e86c0988a6239913f5cb5240252a
Sunali <sunaliseth01@gmail.com>
2018-01-02 14:38:46 +0000

    Test passes for longest_word_in_array

--- lib/questions.rb
+++ lib/questions.rb
@@ -136,6 +136,7 @@ end
 
 # return the longest word in an array
 def longest_word_in_array(array)
+    array.max{|a,b| a.length <=> b.length}
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
