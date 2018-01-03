cfbc9949b46434d8894de9d6afff1747fbfe1a69
Sunali <sunaliseth01@gmail.com>
2018-01-03 14:37:32 +0000

    Test passes bonus questions call_method_from_string

--- lib/questions.rb
+++ lib/questions.rb
@@ -198,6 +198,7 @@ end
 # called call_method_from_string('foobar')
 # the method foobar should be invoked
 def call_method_from_string(str_method)
+    str_method = str_method.call
 end
 
 # return true if the date is a uk bank holiday for 2014
