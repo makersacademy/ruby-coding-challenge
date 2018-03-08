e04f9efb76dc13c6602aa87210bb03cdec3e0866
Elle H <itsellej@gmail.com>
2018-03-08 13:27:51 +0000

    Create a method which returns the sum of keys and values in a hash

--- lib/questions.rb
+++ lib/questions.rb
@@ -35,6 +35,9 @@ end
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+  sum = 0
+  hash.each {|k, v| sum += (k+v)}
+  puts sum
 end
 
 # turn an array (with an even number of elements) into a hash, by
