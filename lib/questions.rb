cd3b5e2c3f56517fe600dea4e58d99dfb8e5e693
freya <freyab_london@yahoo.co.uk>
2018-03-08 22:35:56 +0000

    Solution Question 19

--- lib/questions.rb
+++ lib/questions.rb
@@ -109,8 +109,9 @@ def all_elements_except_first_3(array)
   array.drop(3)
 end
 
-# add an element to the beginning of an array
+# Question 19: add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+  array.unshift(element)
 end
 
 # return the shortest word in an array
