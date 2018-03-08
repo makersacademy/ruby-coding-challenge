3632f72e4935caedc879fb961e3cef8f918f4e75
freya <freyab_london@yahoo.co.uk>
2018-03-08 21:56:50 +0000

    Solution Question 9

--- lib/questions.rb
+++ lib/questions.rb
@@ -46,14 +46,15 @@ def remove_capital_letters_from_string(string)
 end
 
 
-# cut strings in half, and return the first half, e.g.
+# Question 8: cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
 end
 
-# convert a symbol into a string
+# Question 9: convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 
