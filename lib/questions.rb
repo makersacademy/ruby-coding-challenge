e795dcd45439858ea0dc8b1860262242b6cb7a24
freya <freyab_london@yahoo.co.uk>
2018-03-08 21:47:58 +0000

    Solution Question 4

--- lib/questions.rb
+++ lib/questions.rb
@@ -16,10 +16,11 @@ def make_numbers_negative(number)
   0 - number.abs
 end
 
-# swap the keys and values in a hash. e.g.
+# Question 4: swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+  hash.invert
 end
 
 # in a hash where the keys and values are all numbers
