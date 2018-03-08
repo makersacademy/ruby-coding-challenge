37c15c1d662ba3407da994147915dc45b80d21f8
freya <freyab_london@yahoo.co.uk>
2018-03-08 21:43:02 +0000

    Solution Question 2

--- lib/questions.rb
+++ lib/questions.rb
@@ -4,9 +4,10 @@ def round_up_number(float)
   float.ceil
 end
 
-# round down a float up and convert it to an Integer,
+# Question 2: round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.to_i
 end
 
 
