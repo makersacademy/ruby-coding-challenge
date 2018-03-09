c9b28e539dcf7288a6060ff581e5845ab358ea66
Laura Enria <lauraenria@users.noreply.github.com>
2018-03-09 13:23:32 +0000

    solution' s exercises and approach explanation
    9-3-2018

---
+++ lib/approach.md
@@ -0,0 +1,37 @@
+# Approach Used
+
+* I read the exercise very well 
+
+* I looked at the expected solution on the spec to help me understand better.  
+
+* I then tried to work on the logic, and I broke the problem in little steps, before with  Imperative programming  and then functional programming( with methods)
+
+* where possible I did some refactoring.  
+
+* I started from what I already knew from Javascript
+
+* I worked on repl.it
+
+* I run the tests with the command  rspec spec\questions_spec.rb
+
+* I read the wrong outputs on the console. I tried to fix it and I started again. 
+
+* I will try the bonus 
+
+* I fixed a mistake on the test 
+```ruby
+   it 'select_elements_starting_with_vowel' do
+    n = select_elements_starting_with_vowel %w[john david omar fred idriss angela]
+    expect(n).to eq %[omar idriss angela']
+  end
+
+  ```
+
+  ``` ruby
+  expect(n).to eq %[omar idriss angela]
+
+  ```
+
+
+
+* END - HAPPY! || HAPPY END!
--- lib/questions.rb
+++ lib/questions.rb
@@ -1,59 +1,78 @@
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+    float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+    float.floor
 end
 
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
-end
+    (if number < 0 then number else -(number)  end)
+ end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+    hash.invert 
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
-end
+    values = hash.values.inject { |a, b| a + b }
+    keys = hash.keys.inject { |a, b| a + b }
+    values + keys
+  end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+    Hash[*array] 
 end
 
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
-end
+    array = string.split("")
+    result = array.select do |chart|
+      chart == chart.downcase
+    end
+    result.join("")
+    end
 
 
 # cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-end
+    half = string.length/2
+    string.length%2 == 0 ? string[0...half] : string[0...half+1]
+  end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+    return symbol.to_s
 end
 
 
 # get the domain name *without* the .com part, from an email address
 # so onboarding@makersacademy.com becomes makersacademy
 def get_domain_name_from_email_address(email)
-end
+    at_index = email.index("@")
+    dot_index = email.index(".", at_index) # at_index  parameter optional that say where you want to start to search
+    email.slice!(at_index+1...dot_index)
+  end
 
 # capitalize the first letter in each word of a string,
 #  except 'a', 'and' and 'the'
@@ -61,61 +80,105 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
-end
+  
+    def capitalize_string(word)
+      word[0].capitalize +  word.slice(1, word.length)
+    end
+      
+    array = string.split(" ")
+      
+    array_all_up = array.map do |word| # or collect
+        if word != 'a' && word != 'and' && word != 'the'
+        capitalize_string(word)
+        else word
+    end  
+    end
+    
+    string = array_all_up.join(" ")
+    capitalize_string(string)
+    end
+    
+  titleize_a_string('the lion the witch and the wardrobe')
 
 # return true if a string contains any special characters
 # where 'special character' means anything apart from the letters
 # a-z (uppercase and lower) or numbers
 def check_a_string_for_special_characters(string)
-end
+    return !!string.match(/[^0-9A-Za-z]/)
+  end
 
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+    array.select {|word| word[0] == "a"}
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
-end
+    array.select {|word| word[0].match(/[aeiou]/)}.join(" ")  
+    end
 
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+    array.select {|value| !value.nil?}
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+    array.select {|value| !value.nil? && value != false}
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+    array.map {|word| word.reverse!}
 end
 
 # discard the first 3 elements of an array,
 # e.g. [1, 2, 3, 4, 5, 6] becomes [4, 5, 6]
 def all_elements_except_first_3(array)
+    array.slice(3, array.length)
 end
 
 # add an element to the beginning of an array
 def add_element_to_beginning_of_array(array, element)
+    array.unshift(element)
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
-end
+    solution = array[0]
+    for word in array
+    if word.length < solution.length
+      solution = word
+    end
+  end
+  
+  return solution
+  end
 
-# return the shortest word in an array
+# return the longest word in an array
 def longest_word_in_array(array)
-end
+    solution = array[0]
+    for word in array
+    if word.length > solution.length
+      solution = word
+    end
+  end
+  
+  return solution
+  end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+    array.reduce(0, :+)
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 17
 def average_of_array(array)
+    ((array.reduce(0, :+)/(array.length).to_f)).round
 end
 
 # get all the elements in an array, up until the first element
@@ -123,6 +186,13 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+    solution = []
+i = 0 
+while array[i] <= 5  do
+    solution.push(array[i])
+   i +=1
+end
+solution
 end
 
 # get all the letters used in an array of words and return
@@ -130,28 +200,33 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
-end
+    array.join("").split("").sort
+  end
 
 # BONUS SECTION
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+    date.strftime("%d/%m/%Y")
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
 # should return 20
 def get_upper_limit_of(range)
+    range.end
 end
 
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+    range.exclude_end?
 end
 
 # get the square root of a number
 def square_root_of(number)
-end
+    Math.sqrt(number)
+  end
 
 # --- tougher ones ---
 
@@ -159,7 +234,8 @@ end
 # called call_method_from_string('foobar')
 # the method foobar should be invoked
 def call_method_from_string(str_method)
-end
+    eval str_method
+  end
 
 # return true if the date is a uk bank holiday for 2014
 # the list of bank holidays is here:
@@ -176,6 +252,7 @@ end
 
 # count the number of words in a file
 def word_count_a_file(file_path)
+    File.read("file_path")
 end
 
 
