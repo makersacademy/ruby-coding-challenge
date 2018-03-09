b01499b4a8e99730b27c0404932557d8341e422a
AmyDe <35347912+AmyDe@users.noreply.github.com>
2018-03-09 19:31:19 +0000

    Question 8

--- lib/questions.rb
+++ lib/questions.rb
@@ -59,6 +59,7 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  return string[0..(string.length - 1) / 2]
 end
 
 # convert a symbol into a string
