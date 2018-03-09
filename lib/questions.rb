8de90bebeb764dc91e36bd543e2766e6ab812fcf
benji1304 <benjaminstokes@gmail.com>
2018-03-09 08:47:30 +0000

    Tests 2 and 3 done

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,5 +1,3 @@
-# round up a float up and convert it to an Integer,
-# so 3.214 becomes 4
 def round_up_number(float)
   float.ceil.to_i
 end
@@ -7,12 +5,17 @@ end
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+  float.floor.to_i
 end
 
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  if number < 0
+      number.to_i
+  else number.to_i * -1
+  end
 end
 
 # swap the keys and values in a hash. e.g.
