f97308f8edc5e7cdc49a3c6ec7febfdbec2f4878
AmyDe <35347912+AmyDe@users.noreply.github.com>
2018-03-09 21:57:17 +0000

    Bonus 3

--- lib/questions.rb
+++ lib/questions.rb
@@ -214,6 +214,7 @@ end
 # should return true for a 3 dot range like 1...20, false for a
 # normal 2 dot range
 def is_a_3_dot_range?(range)
+    return range.to_s.include?("...")
 end
 
 # get the square root of a number
