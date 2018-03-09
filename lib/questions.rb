b968772831cda885d7521d05c663d180be6a5885
Caterina <superhawkie@yahoo.co.jp>
2018-03-09 13:55:21 +0000

    Update questions.rb

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,42 +1,35 @@
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
-  float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
-  float.floor
 end
 
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
-  number > 0 ? -number : number  
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
-def swap_keys_and_values_in_a_hash(hash)
-  hash.invert  
+def swap_keys_and_values_in_a_hash(hash)  
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
-def add_together_keys_and_values(hash)
-  newArray = hash.inject(:+)
-	newArray.inject(:+)  
+def add_together_keys_and_values(hash) 
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
-def convert_array_to_a_hash(array)
-	newHash = Hash[*array]  
+def convert_array_to_a_hash(array) 
 end
 
 
