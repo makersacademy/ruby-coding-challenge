52bd7b5facf5f7c77a880cf19247fee3feff4240
AmyDe <35347912+AmyDe@users.noreply.github.com>
2018-03-09 21:16:41 +0000

    Question 22
    Found method 'reduce' on ruby doc after some searching and :+ was given as an example.

--- lib/questions.rb
+++ lib/questions.rb
@@ -169,6 +169,7 @@ end
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+    array.reduce(:+)
 end
 
 # get the average from an array, rounded to the nearest integer
