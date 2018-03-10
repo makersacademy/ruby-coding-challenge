96c9c51565378ecdb3cd78cc4a2f86c6bd7d3d38
Caterina <superhawkie@yahoo.co.jp>
2018-03-10 14:19:47 +0000

    Colindale Bonus1

--- lib/questions.rb
+++ lib/questions.rb
@@ -175,7 +175,7 @@ end
 
 # BONUS SECTION
 
-#26 take a date and format it like dd/mm/yyyy, so Halloween 2013
+#B1 take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
 	date.strftime("%d/%m/%y")	
