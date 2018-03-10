6f864587064df41bec4607e829226186da492ed6
benji1304 <benjaminstokes@gmail.com>
2018-03-10 11:59:20 +0000

    redone Q5 updated approach.md

--- approach.md
+++ approach.md
@@ -1,2 +1,8 @@
 Using irb for command line testing of each test
 Checking questions_spec.rb for test requirements
+
+Q5. Summing hash keys and values
+Found a similar solution through StackOverflow. Worked but I think it uses a Rails approach which then failed the rspec test. 
+Did some more searching and practicing with irb and the inject method. 
+Found it was simpler to pull out the keys and values separately, uses the inject method, then sum them separately. 
+
--- lib/questions.rb
+++ lib/questions.rb
@@ -27,15 +27,16 @@ end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
-# {1 => 1, 2 => 2} becomes 6
+#   becomes 6
 def add_together_keys_and_values(hash)
-  hash.sum {|key,value| key + value}
+  hash.keys.inject(:+) + hash.values.inject(:+)
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+  Hash[*array.flatten]
 end
 
 
