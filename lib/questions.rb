ea776f93392ecef3705f5501a63d75c88b2fdd1d
Antonio D'Aria <antoniodaria@msn.com>
2018-03-10 21:40:47 +0000

    Implement get first half of string

--- lib/questions.rb
+++ lib/questions.rb
@@ -45,7 +45,7 @@ end
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
-  string.gsub /[A-Z]/, ""
+  string.delete("A-Z")    #string.gsub /[A-Z]/, ""
 end
 
 
@@ -53,6 +53,7 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+  string[0..(string.length - 1) / 2]
 end
 
 # convert a symbol into a string
