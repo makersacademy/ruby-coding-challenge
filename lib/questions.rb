7722ccc17d2ace135d8df3483636de60a8c6b7f6
Elle H <itsellej@gmail.com>
2018-03-10 17:03:04 +0000

    Refactor Q8 - cut string in half

--- lib/questions.rb
+++ lib/questions.rb
@@ -69,13 +69,7 @@ end
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
-  num = string.length
-  if num.odd?
-    halfnum = (num.to_f/2).ceil
-  else
-    halfnum = (num.to_f/2)
-  end
-  string[0,halfnum]
+  string[0, (string.length.to_f / 2).ceil]
 end
 
 
