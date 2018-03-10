ce1353eebbae12f41d1bec64314d39ae719954e6
Caterina <superhawkie@yahoo.co.jp>
2018-03-10 00:48:08 +0000

    Colindale Q8

--- lib/questions.rb
+++ lib/questions.rb
@@ -47,10 +47,14 @@ def remove_capital_letters_from_string(string)
 end
 
 
-# cut strings in half, and return the first half, e.g.
+#8 cut strings in half, and return the first half, e.g.
 # 'banana' becomes 'ban'. If the string is an odd number of letters
 # round up - so 'apple' becomes 'app'
 def get_first_half_of_string(string)
+	#str = "cisondivalmarino!"
+	#(str.length / 2).ceil # 8
+	i = (string.length / 2.to_f).ceil # 9
+	string[0, i + 1]	
 end
 
 # convert a symbol into a string
