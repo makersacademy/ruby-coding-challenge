7953bc6a814c0b9059c39e96339952a2b78b130c
Caterina <superhawkie@yahoo.co.jp>
2018-03-10 00:53:35 +0000

    Colindale Q14

--- lib/questions.rb
+++ lib/questions.rb
@@ -90,8 +90,10 @@ def select_elements_starting_with_a(array)
 	array.select {| i | i[0] == "a"}	
 end
 
-# keep only the elements that start with a vowel
+#14 keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+	#cty = ["america", "britain", "canada", "australia", "estonia"]
+	array.select{|i| i[0].match(/^[aeiou]/)}	
 end
 
 
