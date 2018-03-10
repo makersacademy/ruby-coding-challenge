26ef65e170deb35f671ed272cfe2ace8c4cc2ec0
Caterina <superhawkie@yahoo.co.jp>
2018-03-10 00:42:19 +0000

    Colindale Q4

--- lib/questions.rb
+++ lib/questions.rb
@@ -20,7 +20,8 @@ end
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
-def swap_keys_and_values_in_a_hash(hash)  
+def swap_keys_and_values_in_a_hash(hash)
+  hash.invert  
 end
 
 # in a hash where the keys and values are all numbers
