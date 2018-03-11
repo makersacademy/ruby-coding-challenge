42e121ac6cf453a18567737c426f267a4bffdd81
freya <freyab_london@yahoo.co.uk>
2018-03-11 14:35:30 +0000

    Solution Bonus 1

--- lib/questions.rb
+++ lib/questions.rb
@@ -189,6 +189,7 @@ end
 # Bonus 1: take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+  date.strftime("%d/%m/%Y")
 end
 
 # Bonus 2: get the upper limit of a range. e.g. for the range 1..20, you
