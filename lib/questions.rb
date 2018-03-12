d5e9b749653534b4e9fe455b9bb1206de499d886
Ellie_404 <micheasam@gmail.com>
2018-03-12 00:42:07 +0000

    attemped questions 3 and 9. attempts incomplete

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,17 +1,23 @@
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+
 end
 
 
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+  if number > 0
+    number * -1
+  else number * 1
+  end
 end
 
 # swap the keys and values in a hash. e.g.
@@ -47,6 +53,7 @@ end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+  symbol.to_s
 end
 
 
@@ -61,6 +68,7 @@ end
 # 'the lion the witch and the wardrobe' becomes
 # 'The Lion the Witch and the Wardrobe'
 def titleize_a_string(string)
+
 end
 
 # return true if a string contains any special characters
