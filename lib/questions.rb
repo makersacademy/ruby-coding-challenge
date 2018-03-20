8672a6b8f9cbf33d66ea28d0b20597ef3d87d19e
Irina <igiosanu29@gmail.com>
2018-03-20 22:07:01 +0000

    Add together keys and values

--- lib/questions.rb
+++ lib/questions.rb
@@ -28,6 +28,9 @@ end
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+	sum = 0
+	hash.each{|key, value| sum += key + value}
+	sum
 end
 
 # turn an array (with an even number of elements) into a hash, by
