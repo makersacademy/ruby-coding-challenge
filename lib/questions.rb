507df3e44ef8b61a497c3cf6d2226306328ce5e9
jtsalva <tim@jtsalva.dev>
2019-09-19 17:20:16 +0100

    format_date_nicely

--- lib/questions.rb
+++ lib/questions.rb
@@ -253,6 +253,7 @@ end
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+  date.strftime("%d/%m/%Y")
 end
 
 # get the upper limit of a range. e.g. for the range 1..20, you
