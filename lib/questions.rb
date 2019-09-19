380cf9ef9cccc5bedd1090e387c9aa282a2dafdd
jtsalva <tim@jtsalva.dev>
2019-09-19 16:35:12 +0100

    better longest_word_in_array

--- lib/questions.rb
+++ lib/questions.rb
@@ -191,15 +191,7 @@ end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
-  longest_word_index = 0
-
-  array.each_with_index do |word, index|
-    if word.length > array[longest_word_index].length
-      longest_word_index = index
-    end
-  end
-
-  array[longest_word_index]
+  array.max_by(&:length)
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
