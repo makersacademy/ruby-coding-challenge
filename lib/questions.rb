d9397390cc8181a6d82c2df8a8df52e0ee6676fe
Lois K. Lee <lois.lee@me.com>
2020-03-10 21:12:37 -0400

    round number up, round number down

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,11 +1,13 @@
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+    float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+    float.floor
 end
 
 
